f4b9663550a0664b075315522d36ff7c93195b74
Borkabori <birgesborbala@gmail.com>
2022-01-08 23:35:40 +0100

    Form searching for a word, alerts it

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import stareyes from "./pictures/stareyes.png";
+import Dictionary from "./Dictionary";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={stareyes} className="stareyes-logo" alt="stareyes" />
-        <a href="#" className="btn btn-dark">
-          {" "}
-          Hello{" "}
-        </a>
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={stareyes} className="stareyes-logo" alt="stareyes" />
+          <a href="#" className="btn btn-dark">
+            {" "}
+            Hello{" "}
+          </a>
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="text-center">Coded by Borbála Birgés</footer>
+      </div>
     </div>
   );
 }
