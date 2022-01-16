382f8792274ec0c0857649a4ad5cf2f7da95204d
Borkabori <birgesborbala@gmail.com>
2022-01-16 18:28:04 +0100

    Images of the seched word got visible

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,14 +2,21 @@ import React, { useState } from "react";
 import axios from "axios";
 import "./Dictionary.css";
 import Results from "./Results";
+import Photos from "./Photos";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState(null);
   let [wordResults, setWordResults] = useState(null);
+  let [photos, setPhotos] = useState(null);
 
   function handleResponse(response) {
     setWordResults(response.data[0]);
     // console.log(response.data[0].meanings[0].definitions[0].definition);
+  }
+
+  function handlePexelsResponse(response) {
+    // console.log(response.data);
+    setPhotos(response.data.photos);
   }
 
   function search(event) {
@@ -17,6 +24,13 @@ export default function Dictionary() {
     // where it came from: https://dictionaryapi.dev/
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
+
+    const pexelsApiKey =
+      "563492ad6f917000010000010f09483a61744624b3879362169fa2a4";
+
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=3`;
+    const headers = { Authorization: `Bearer ${pexelsApiKey}` };
+    axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
   }
 
   function handleKeywordChange(event) {
@@ -39,6 +53,7 @@ export default function Dictionary() {
         </form>
       </div>
       <Results outcome={wordResults} />
+      <Photos photos={photos} />
     </div>
   );
 }
